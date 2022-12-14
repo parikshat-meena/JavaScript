71a70d4644b4722318130b0d4aac1a460a98ee2b
Parikshat Meena <meenaparikshit2512@gmail.com>
2022-12-14 22:46:59 +0530

    case study JavaScript

---
+++ Case Study JS/app.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $("#mega").hover(            
+//         function() {
+//             $('.megaDropDown', this).not('.megaDropDown').stop(true,true).slideDown("400");
+//             $(this).toggleClass('open');        
+//         },
+//         function() {
+//             $('.megaDropDown', this).not('.megaDropDown').stop(true,true).slideUp("400");
+//             $(this).toggleClass('open');       
+//         }
+//     );
+// }
